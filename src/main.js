9a6f83338ca1411a0b06e0efd82e6126c89b4aeb
young3kin <850787539@qq.com>
2019-03-24 23:57:37 +0800

    Take-out项目搭建

---
+++ src/main.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+// 入口JS
+import Vue from 'vue'
+import App from './App.vue'
+
+// eslint-disable-next-line no-new
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
---
+++ src/pages/Search/Serch.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+Serch
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang='stylus' rel='stylesheet/stylus'>
+</style>
